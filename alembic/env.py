c149e1b9d5c70e6855f985b8cecfd1a4a38ba992
retr0-spection <ratinailana@icloud.com>
2024-09-15 00:55:50 +0200

    save

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,7 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -19,10 +20,12 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 # Import your model's MetaData object here
-from app.database import Base  # Change this to the actual import path
+from app.database import Base, SQLALCHEMY_DATABASE_URL  # Change this to the actual import path
+
 target_metadata = Base.metadata  # This should be your metadata object
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
+config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
--- app/database.py
+++ app/database.py
@@ -3,9 +3,27 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from fastapi import Depends
+from functools import lru_cache
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    DEBUG: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+SQLALCHEMY_DATABASE_URL =  "sqlite:///./sql_app.db" if int(get_settings().DEBUG) else "postgres://rag_muddy_fog_9690:f5yr0NFXEEB39OL@posidon.flycast:5432/rag_muddy_fog_9690?sslmode=disable"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
